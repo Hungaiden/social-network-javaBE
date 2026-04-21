ab3e8c961cc38f066d89c179dbc65eb2a0810e19
Bui Duc Hung <duchungbui1@gmail.com>
2026-04-21 16:12:39 +0700

    fix: register:

--- FakeBook/src/main/java/com/example/FakeBook/Controller/AuthController.java
+++ FakeBook/src/main/java/com/example/FakeBook/Controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.FakeBook.Controller;
 import com.example.FakeBook.DTO.Request.AuthenticationRequest;
+import com.example.FakeBook.DTO.Request.UserCreationRequest;
 import com.example.FakeBook.DTO.Response.ApiResponse;
 import com.example.FakeBook.DTO.Response.AuthenticationResponse;
 import com.example.FakeBook.DTO.Response.UserDetailResponse;
 import com.example.FakeBook.facade.AuthFacadeService;
 import com.nimbusds.jose.JOSEException;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -14,13 +16,25 @@ import java.text.ParseException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "auth")
-
 public class AuthController {
     @Autowired
     private AuthFacadeService authFacadeService;
 
+    @Operation(
+            summary = "Dang ky tai khoan",
+            description = "Dang ky tai khoan moi va tu dong dang nhap"
+    )
+    @PostMapping("/register")
+    public ApiResponse<AuthenticationResponse> register(@Valid @RequestBody UserCreationRequest request) {
+        return ApiResponse.<AuthenticationResponse>builder()
+                .code(1000)
+                .message("Dang ky thanh cong!")
+                .Result(authFacadeService.register(request))
+                .build();
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/login")
     public ApiResponse<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
         return ApiResponse.<AuthenticationResponse>builder()
@@ -29,6 +43,7 @@ public class AuthController {
                 .build();
     }
 
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/logout")
     public ApiResponse<Void> logout() throws ParseException, JOSEException {
         authFacadeService.logout();
@@ -38,6 +53,7 @@ public class AuthController {
                 .build();
     }
 
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/myInfo")
     public ApiResponse<UserDetailResponse> myInfo() throws ParseException, JOSEException {
         return ApiResponse.<UserDetailResponse>builder()
